21630113752518517be6a4cc92ad86fb763985f7
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2021-05-14 04:47:43 +0530

    Added Provider

--- lib/componentsOnOff.dart
+++ lib/componentsOnOff.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+// var finderOpen = Provider.of<OnOff>(context).getFinder;
+// Provider.of<OnOff>(context, listen: false).toggleFinder();
 
 class OnOff extends ChangeNotifier{
   bool finderOpen =false;
+  bool ccOpen =true;
 
   bool get getFinder {
     return finderOpen;
@@ -11,6 +14,15 @@ class OnOff extends ChangeNotifier{
 
   void toggleFinder() {
     finderOpen= !finderOpen;
+    notifyListeners();
+  }
+
+  bool get getCc {
+    return ccOpen;
+  }
+
+  void toggleCc() {
+    ccOpen= !ccOpen;
     notifyListeners();
   }
 }
