1c3bb97c290424963bff927c3fa2ba71b0630590
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2021-05-24 09:05:05 +0530

    Updated Theme

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -6,9 +6,17 @@ class ThemeNotifier with ChangeNotifier {
   // static bool isDark;
 
   static final ThemeData lightTheme = ThemeData(
+    textTheme: TextTheme(
+        headline4: TextStyle(
+          fontWeight: FontWeight.w600,
+          fontSize: 12,
+        )
+    ),
     primarySwatch: Colors.blueGrey,
-    backgroundColor: Colors.white.withOpacity(0.3), //Control Center
-    cardColor: Colors.blueGrey[50],
+    backgroundColor: Colors.white.withOpacity(0.2), //Control Center
+    cardColor: Colors.black.withOpacity(0.0), //Control Center item border, font color
+    accentColor: Colors.black.withOpacity(.3), //shadow color
+    focusColor: Colors.white.withOpacity(0.4), //docker color
     primaryTextTheme: TextTheme(
       button: TextStyle(
         color: Colors.blueGrey,
@@ -25,10 +33,19 @@ class ThemeNotifier with ChangeNotifier {
   );
 
   static final ThemeData darkTheme = ThemeData(
+    textTheme: TextTheme(
+      headline4: TextStyle(
+        fontWeight: FontWeight.w500,
+        fontSize: 12,
+      )
+    ),
     primarySwatch: Colors.deepPurple,
-      backgroundColor: Color(0xff2b2b2b).withOpacity(0.25), //Control Center
-    cardColor: Colors.black.withOpacity(0.2), //Control Center item border
-    scaffoldBackgroundColor: Color(0xff242b4b),
+      backgroundColor: Color(0xff2b2b2b).withOpacity(.1), //Control Center
+    cardColor: Colors.white.withOpacity(0.2), //Control Center item border, font color
+      accentColor: Colors.black.withOpacity(.4), //shadow color
+      focusColor: Color(0xff6c5252).withOpacity(0.3), //docker color
+
+      scaffoldBackgroundColor: Color(0xff242b4b),
     primaryTextTheme: TextTheme(
       button: TextStyle(
         color: Colors.blueGrey[200],
--- lib/widgets.dart
+++ lib/widgets.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 
 class MBPText extends StatelessWidget {
+  final Color color;
   final String text;
-  const MBPText({this.text, Key key}) : super(key: key);
+  const MBPText({this.color= Colors.black,this.text, Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return FittedBox(
       fit: BoxFit.scaleDown,
       child: Text(text,style: TextStyle(
-        fontWeight: FontWeight.w600,
-        color: Colors.black,
+        fontWeight: Theme.of(context).textTheme.headline4.fontWeight,
+        color: color,
         fontSize: 12,
       ),textAlign: TextAlign.center,),
     );
