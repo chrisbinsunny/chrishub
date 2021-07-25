00827baf14c8b1247822f4eee315d32496420bb4
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2021-07-26 04:26:55 +0530

    Added running clock

--- lib/fileMenu/fileMenu.dart
+++ lib/fileMenu/fileMenu.dart
@@ -21,12 +21,10 @@ class FileMenu extends StatefulWidget {
 
 class _FileMenuState extends State<FileMenu> {
   var rand = new Random();
-  DateTime now;
   int num;
 
   @override
   void initState() {
-    now = DateTime.now();
     num= rand.nextInt(20);
     super.initState();
   }
@@ -94,22 +92,22 @@ class _FileMenuState extends State<FileMenu> {
                     SizedBox(width: size.width*0.014,),
                     Image.asset("assets/icons/siri.png", height: 15),
                     SizedBox(width: size.width*0.014,),
-                    FittedBox(fit: BoxFit.fitHeight,
-                      child: Text(
-                        "${DateFormat('E d LLL hh:mm a').format(now)}",
-                        style: TextStyle(fontFamily: 'HN',
-                            fontWeight: FontWeight.w400,
-                            color: Colors.white),),),
-                    // StreamBuilder(
-                    //   stream: Stream.periodic(const Duration(seconds: 1)),
-                    //   builder: (context, snapshot){
-                    //   return FittedBox(fit: BoxFit.fitHeight,
-                    //     child: Text(
-                    //       "${DateFormat('E d LLL hh:mm a').format(DateTime.now())}",
-                    //       style: TextStyle(fontFamily: 'SF',
-                    //           fontWeight: FontWeight.w400,
-                    //           color: Colors.white),),);
-                    // },),
+                    // FittedBox(fit: BoxFit.fitHeight,
+                    //   child: Text(
+                    //     "${DateFormat('E d LLL hh:mm a').format(DateTime.now())}",
+                    //     style: TextStyle(fontFamily: 'HN',
+                    //         fontWeight: FontWeight.w400,
+                    //         color: Colors.white),),),
+                    StreamBuilder(
+                      stream: Stream.periodic(const Duration(seconds: 1)),
+                      builder: (context, snapshot){
+                      return FittedBox(fit: BoxFit.fitHeight,
+                        child: Text(
+                          "${DateFormat('E d LLL hh:mm a').format(DateTime.now())}",
+                          style: TextStyle(fontFamily: 'HN',
+                              fontWeight: FontWeight.w400,
+                              color: Colors.white),),);
+                    },),
                     SizedBox(width: size.width*0.014,),
                   ],
                 ),
