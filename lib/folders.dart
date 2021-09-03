444b2fd4a3abf4df37869e2ac88bc4b0a8d1a0f9
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2021-09-04 01:43:54 +0530

    Folder feedback added

--- lib/folders.dart
+++ lib/folders.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mac_dt/widgets.dart';
 import 'package:provider/provider.dart';
@@ -54,9 +57,11 @@ class Folder extends StatefulWidget {
 
 class _FolderState extends State<Folder> {
   Offset position= Offset(200, 150);
-  TextEditingController controller = new TextEditingController(text: "Untitled Folder");
+  TextEditingController controller = new TextEditingController(text: "untitled folder");
   final _focusNode = FocusNode();
-  bool selected= false;
+  bool pan= false;
+  bool bgVisible= false;
+
 
   @override
   void initState() {
@@ -75,65 +80,124 @@ class _FolderState extends State<Folder> {
 
   @override
   Widget build(BuildContext context) {
-    selected= false;
-    print
-    return Positioned(
-      top: position.dy,
-      left: position.dx,
-      child: GestureDetector(
-        onPanUpdate: (tapInfo){
-          setState(() {
-            position = Offset(position.dx + tapInfo.delta.dx,
-                position.dy + tapInfo.delta.dy);
-          });
-        },
-        onTap: (){
-          setState(() {
-            selected=true;
-          });
-        },
-        child: Container(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Image.asset("assets/icons/folder.png", height: screenHeight(context, mulBy: 0.1), width: screenWidth(context, mulBy: 0.06),),
-              widget.renaming?
-                  Container(
-                    height: screenHeight(context, mulBy: 0.025),
-                    width: screenWidth(context, mulBy: 0.06),
-                    child: TextField(
-                      controller: controller,
-                      autofocus: true,
-                      focusNode: _focusNode,
-                      textAlign: TextAlign.center,
-                      decoration: InputDecoration(
-                        isDense: true,
-                        contentPadding: EdgeInsets.all(0),
-                        border: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                        enabledBorder: InputBorder.none,
-                        errorBorder: InputBorder.none,
-                        disabledBorder: InputBorder.none,
-                      ),
-                      cursorColor: Colors.black,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontFamily: "HN",
-                        fontWeight: FontWeight.w500,
-                        fontSize: 12
-                      ),
-                      onSubmitted: (s){
-                        setState(() {
-                          widget.renaming=false;
-                          widget.name=controller.text.toString();
-                        });
-                      },
-                    ),
-                  )
-              :MBPText(text: widget.name, color: Colors.white, fontFamily: "HN", weight: FontWeight.w500, size: 12,),
-            ],
+    return Container(
+      height: screenHeight(context),
+      width: screenWidth(context),
+      child: Stack(
+        children: [
+          Visibility(
+            visible: bgVisible,
+            child: Positioned(
+              top: widget.initPos.dy,
+              left: widget.initPos.dx,
+              child: Container(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Image.asset("assets/icons/folder.png", height: screenHeight(context, mulBy: 0.1), width: screenWidth(context, mulBy: 0.06),),
+                    MBPText(text: widget.name, color: Colors.white, fontFamily: "HN", weight: FontWeight.w500, size: 12,),
+                  ],
+                ),
+              ),
+            ),
           ),
-        ),
+          AnimatedPositioned(
+            duration: Duration(milliseconds: pan?0:200),
+            top: position.dy,
+            left: position.dx,
+            child: GestureDetector(
+              onPanUpdate: (tapInfo){
+                setState(() {
+                  position = Offset(position.dx + tapInfo.delta.dx,
+                      position.dy + tapInfo.delta.dy);
+                });
+              },
+              onPanDown: (e){
+                setState(() {
+                  widget.renaming=false;
+                  widget.name=controller.text.toString();
+                  pan=true;
+                  bgVisible=true;
+                });
+              },
+              onPanEnd: (e){
+                setState(() {
+                  pan=false;
+                  position=widget.initPos;
+                });
+                Timer(Duration(milliseconds: 200), (){setState(() {
+                  bgVisible=false;
+                });});
+              },
+
+              child: Container(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                        height: screenHeight(context, mulBy: 0.1),
+                        width: screenWidth(context, mulBy: 0.048),
+                        decoration: widget.renaming?BoxDecoration(
+                          color: Colors.black.withOpacity(0.25),
+                            border: Border.all(
+                                color: Colors.grey.withOpacity(0.4),
+                              width: 2
+                            ),
+                            borderRadius: BorderRadius.circular(4)
+                        ):BoxDecoration(),
+                        child: Image.asset("assets/icons/folder.png", height: screenHeight(context, mulBy: 0.095), width: screenWidth(context, mulBy: 0.059),)),
+                    SizedBox(height: screenHeight(context, mulBy: 0.005), ),
+                    widget.renaming?
+                    Container(
+                      height: screenHeight(context, mulBy: 0.024),
+                      width: screenWidth(context, mulBy: 0.06),
+                      decoration: BoxDecoration(
+                        color: Color(0xff1a6cc4).withOpacity(0.7),
+                        border: Border.all(
+                          color: Colors.blueAccent
+                        ),
+                        borderRadius: BorderRadius.circular(3)
+                      ),
+                      child: Theme(
+                        data: ThemeData(textSelectionTheme: TextSelectionThemeData(
+                            selectionColor: Colors.transparent)),
+                        child: TextField(
+                          controller: controller,
+                          autofocus: true,
+                          focusNode: _focusNode,
+                          textAlign: TextAlign.center,
+                          decoration: InputDecoration(
+                            isDense: true,
+                            contentPadding: EdgeInsets.only(top: 4.5, bottom: 0, left: 0, right: 0),
+                            border: InputBorder.none,
+                            focusedBorder: InputBorder.none,
+                            enabledBorder: InputBorder.none,
+                            errorBorder: InputBorder.none,
+                            disabledBorder: InputBorder.none,
+                          ),
+                          cursorColor: Colors.white60,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: "HN",
+                              fontWeight: FontWeight.w500,
+                              fontSize: 12
+                          ),
+                          onSubmitted: (s){
+                            setState(() {
+                              widget.renaming=false;
+                              widget.name=controller.text.toString();
+                            });
+                          },
+                        ),
+                      ),
+                    )
+                        :MBPText(text: widget.name, color: Colors.white, fontFamily: "HN", weight: FontWeight.w500, size: 12,),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -146,3 +210,4 @@ class _FolderState extends State<Folder> {
 //
 //   FolderProps({this.name="", this.initPos, this.renaming= false});
 // }
+
