463d4688b2bc72bb588ead724a34096430e948fe
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2021-11-13 02:56:48 +0530

    NS Blender

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
 class MBPText extends StatelessWidget {
   final Color color;
@@ -112,5 +114,51 @@ class _MinimiserState extends State<Minimiser> {
 
       child: widget.child,
     );
+  }
+}
+
+
+///Night Shift Blender
+class BlendMask extends SingleChildRenderObjectWidget {
+  final BlendMode blendMode;
+  final double opacity;
+
+  BlendMask({
+    @required this.blendMode,
+    this.opacity = 1.0,
+    Key key,
+    Widget child,
+  }) : super(key: key, child: child);
+
+  @override
+  RenderObject createRenderObject(context) {
+    return RenderBlendMask(blendMode, opacity);
+  }
+
+  @override
+  void updateRenderObject(BuildContext context, RenderBlendMask renderObject) {
+    renderObject.blendMode = blendMode;
+    renderObject.opacity = opacity;
+  }
+}
+
+class RenderBlendMask extends RenderProxyBox {
+  BlendMode blendMode;
+  double opacity;
+
+  RenderBlendMask(this.blendMode, this.opacity);
+
+  @override
+  void paint(context, offset) {
+    context.canvas.saveLayer(
+      offset & size,
+      Paint()
+        ..blendMode = blendMode
+        ..color = Color.fromARGB((opacity * 255).round(), 255, 255, 255),
+    );
+
+    super.paint(context, offset);
+
+    context.canvas.restore();
   }
 }
