cdb9e2afd865c89d45dd35207bb83015c530f25b
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2021-11-20 01:59:21 +0530

    Sound fixed

--- lib/iPadOS/fileMenu/controlCentre.dart
+++ lib/iPadOS/fileMenu/controlCentre.dart
@@ -24,7 +24,7 @@ class _ControlCentreState extends State<ControlCentre> {
 
   @override
   void initState() {
-    brightness = 100;
+    brightness = 95.98;
     sound= 35;
     super.initState();
   }
@@ -382,7 +382,7 @@ class _ControlCentreState extends State<ControlCentre> {
                               child: Slider(
                                 value: brightness,
                                 min: 0,
-                                max: 100,
+                                max: 95.98,
                                 onChanged: (val) {
                                   if (val < 6.7) val = 6.7;
                                   setState(() {
@@ -399,11 +399,16 @@ class _ControlCentreState extends State<ControlCentre> {
                         ),
                         IgnorePointer(
                           ignoring: true,
-                          child: Image.asset(
-                            "assets/icons/brightness.png",
-                            height: 15,
-                            color: Colors.black.withOpacity(0.55),
-                          ),
+                          child: Padding(
+                            padding: EdgeInsets.only(
+                              bottom: screenHeight(context, mulBy: 0.015)
+                            ),
+                            child: Icon(
+                              CupertinoIcons.brightness_solid,
+                              size: 30,
+                              color: Colors.black.withOpacity(0.55),
+                            ),
+                          )
                         )
                       ],
                     ),
@@ -411,133 +416,66 @@ class _ControlCentreState extends State<ControlCentre> {
                   SizedBox(
                     width: screenWidth(context, mulBy: 0.0135),
                   ),
-                  Container(
-                    margin: EdgeInsets.zero,
-                    padding: EdgeInsets.symmetric(
-                        horizontal: screenWidth(context, mulBy: 0.01)
-                    ),
-                    height: screenHeight(context, mulBy: 0.18),
-                    width: screenWidth(context, mulBy: 0.0495),
-                    decoration: BoxDecoration(
-                        borderRadius:
-                        BorderRadius.all(Radius.circular(15)),
-                        color: Colors.black.withOpacity(0.4)
-                    ),
-                    child: Container(
-                      decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.white.withOpacity(0.2)
-                      ),
-                      height: screenHeight(context, mulBy: 0.059),
-                      width: screenWidth(context, mulBy: 0.039),
-                      alignment: Alignment.center,
-                      margin: EdgeInsets.only(
-                          right: screenWidth(context, mulBy: 0.007)
-                      ),
+                  ClipRRect(
+                    borderRadius:
+                    BorderRadius.all(Radius.circular(15)),
+                    child: Stack(
+                      clipBehavior: Clip.antiAlias,
+                      alignment: Alignment.bottomCenter,
+                      children: [
+                        Container(
+                          height: screenHeight(context, mulBy: 0.18),
+                          width: screenWidth(context, mulBy: 0.0495),
+                          decoration: BoxDecoration(
+                            borderRadius:
+                            BorderRadius.all(Radius.circular(15)),
+                          ),
+                          child: RotatedBox(
+                            quarterTurns: 3,
+                            child: SliderTheme(
+                              data: SliderTheme.of(context)
+                                  .copyWith(
+                                trackHeight: screenWidth(context, mulBy: 0.0495),
+                                activeTrackColor: Colors.white,
+                                trackShape: RectangularSliderTrackShape(),
+                                inactiveTrackColor: Colors.black.withOpacity(0.4),
+                                thumbShape: RoundSliderThumbShape(enabledThumbRadius: 0),
+                                overlayShape: SliderComponentShape
+                                    .noOverlay,
+                              ),
+                              child: Slider(
+                                value: sound,
+                                min: 0,
+                                max: 95.98,
+                                onChanged: (val) {
+                                  if (val < 6.7) val = 6.7;
+                                  setState(() {
+                                    sound = val;
+                                  });
+                                },
+
+                              ),
+                            ),
+                          ),
+                        ),
+                        IgnorePointer(
+                            ignoring: true,
+                            child: Padding(
+                              padding: EdgeInsets.only(
+                                  bottom: screenHeight(context, mulBy: 0.015)
+                              ),
+                              child: Icon(
+                                CupertinoIcons.speaker_3_fill,
+                                size: 30,
+                                color: Colors.black.withOpacity(0.55),
+                              ),
+                            )
+                        )
+                      ],
                     ),
                   ),
                 ],
               ),
-              // Container(
-              //   decoration: BoxDecoration(
-              //     borderRadius:
-              //     BorderRadius.all(Radius.circular(10)),
-              //     boxShadow: [ccShadow],
-              //   ),
-              //   child: ClipRRect(
-              //     borderRadius:
-              //     BorderRadius.all(Radius.circular(10)),
-              //     child: BackdropFilter(
-              //       filter: ImageFilter.blur(
-              //           sigmaX: 15.0, sigmaY: 15.0),
-              //       child: BrdrContainer(
-              //         height: 0.08,
-              //         width: 1,
-              //         child: Container(
-              //           padding: EdgeInsets.symmetric(
-              //               horizontal: screenWidth(context,
-              //                   mulBy: 0.005),
-              //               vertical: screenHeight(context,
-              //                   mulBy: 0.005)),
-              //           height:
-              //           screenHeight(context, mulBy: 0.08),
-              //           width: screenWidth(
-              //             context,
-              //           ),
-              //           decoration: ccDecoration,
-              //           child: Column(
-              //             mainAxisAlignment:
-              //             MainAxisAlignment.spaceEvenly,
-              //             crossAxisAlignment:
-              //             CrossAxisAlignment.start,
-              //             children: [
-              //               MBPText(
-              //                 overflow: TextOverflow.clip,
-              //                 text: "Sound",
-              //                 color: Theme.of(context)
-              //                     .cardColor
-              //                     .withOpacity(1),
-              //               ),
-              //               Stack(
-              //                 clipBehavior: Clip.antiAlias,
-              //                 alignment: Alignment.centerLeft,
-              //                 children: [
-              //                   SliderTheme(
-              //                     data: SliderTheme.of(context)
-              //                         .copyWith(
-              //                       trackHeight: 15,
-              //                       activeTrackColor: Colors.white,
-              //                       thumbColor: Colors.white,
-              //                       minThumbSeparation: 20,
-              //                       trackShape: SliderTrackShape(),
-              //                       inactiveTrackColor: Colors.white
-              //                           .withOpacity(0.25),
-              //                       thumbShape: RoundSliderThumbShape(
-              //                           enabledThumbRadius: 8.4,
-              //                           elevation: 10,
-              //                           pressedElevation: 20
-              //                       ),
-              //                       overlayShape: SliderComponentShape
-              //                           .noOverlay,
-              //                     ),
-              //                     child: Slider(
-              //                       value: sound,
-              //                       min: 0,
-              //                       max: 100,
-              //                       onChanged: (val) {
-              //                         if (val > 95.98)
-              //                           val = 95.98;
-              //                         else if (val < 6.7) val = 6.7;
-              //                         setState(() {
-              //                           sound = val;
-              //                         });
-              //                       },
-              //                     ),
-              //                   ),
-              //                   IgnorePointer(
-              //                     ignoring: true,
-              //                     child: Row(
-              //                       children: [
-              //                         SizedBox(width: 4,),
-              //                         Image.asset(
-              //                           "assets/icons/sound.png",
-              //                           height: 13,
-              //                           color: Colors.black.withOpacity(0.55),
-              //                         ),
-              //                       ],
-              //                     ),
-              //                   )
-              //                 ],
-              //               ),
-              //
-              //               //CCSlider(height: 16, width: screenWidth(context),),
-              //             ],
-              //           ),
-              //         ),
-              //       ),
-              //     ),
-              //   ),
-              // ),
             ],
           ),
         ),
