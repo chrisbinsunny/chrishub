531916d937fc2dff56a80d514909f7ac30ecb4c0
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-01-13 15:47:44 +0530

    Working on spotify

--- lib/iPadOS/apps/spotify.dart
+++ lib/iPadOS/apps/spotify.dart
@@ -3,6 +3,8 @@ import 'package:flutter/rendering.dart';
 import 'package:mac_dt/system/componentsOnOff.dart';
 import 'package:mac_dt/theme/theme.dart';
 import 'package:provider/provider.dart';
+import 'package:table_calendar/table_calendar.dart';
+import '../../providers.dart';
 import '../../system/openApps.dart';
 import '../../sizes.dart';
 import '../../widgets.dart';
@@ -18,20 +20,17 @@ class Spotify extends StatefulWidget {
 }
 
 class _SpotifyState extends State<Spotify> {
-  Offset position = Offset(0.0, 0.0);
-  bool spotifyFS;
-  bool spotifyPan;
   final html.IFrameElement _iframeElementURL = html.IFrameElement();
 
 
   @override
   void initState() {
-    position = widget.initPos;
     super.initState();
     _iframeElementURL.src = 'https://open.spotify.com/embed/playlist/1hJhyi1Ofxvlsswc1ZeXEs';
     _iframeElementURL.style.border = 'none';
     _iframeElementURL.allow = "autoplay; encrypted-media;";
     _iframeElementURL.allowFullscreen = true;
+    _iframeElementURL.height="100%";
     ui.platformViewRegistry.registerViewFactory(
       'spotifyIframe',
           (int viewId) => _iframeElementURL,
@@ -41,214 +40,35 @@ class _SpotifyState extends State<Spotify> {
 
   @override
   Widget build(BuildContext context) {
-    var spotifyOpen = Provider.of<OnOff>(context).getSpotify;
-    spotifyFS = Provider.of<OnOff>(context).getSpotifyFS;
-    spotifyPan = Provider.of<OnOff>(context).getSpotifyPan;
-    return spotifyOpen
-        ? AnimatedPositioned(
-      duration: Duration(milliseconds: spotifyPan ? 0 : 200),
-      top: spotifyFS ? screenHeight(context, mulBy: 0.0335) : position.dy,
-      left: spotifyFS ? 0 : position.dx,
-      child: spotifyWindow(context),
-    )
-        : Container();
+    return spotifyWindow(context);
   }
 
-  AnimatedContainer spotifyWindow(BuildContext context) {
-    String topApp = Provider.of<Apps>(context).getTop;
-    return AnimatedContainer(
-      duration: Duration(milliseconds: 200),
-      width: spotifyFS
-          ? screenWidth(context, mulBy: 1)
-          : screenWidth(context, mulBy: 0.7),
-      height: spotifyFS
-          ?screenHeight(context, mulBy: 0.966)
-          : screenHeight(context, mulBy: 0.75),
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: Colors.white.withOpacity(0.2),
+  Widget spotifyWindow(BuildContext context) {
+    var scale= Provider.of<DataBus>(context).getScale;
+    return Transform.scale(
+      scale: scale,
+      alignment: Alignment.topCenter,
+      child: Container(
+        width: screenWidth(context, ),
+        height: screenHeight(context, ),
+        decoration: BoxDecoration(
+            color: Theme.of(context).indicatorColor
         ),
-        borderRadius: BorderRadius.all(Radius.circular(10)),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.2),
-            spreadRadius: 10,
-            blurRadius: 15,
-            offset: Offset(0, 8), // changes position of shadow
-          ),
-        ],
-      ),
-      child: Stack(
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Stack(
-                alignment: Alignment.centerRight,
-                children: [
-                  Container(
-                    height: spotifyFS
-                        ? screenHeight(context, mulBy: 0.059)
-                        : screenHeight(context, mulBy: 0.06),
-                    decoration: BoxDecoration(
-                        color: Theme.of(context).dividerColor,
-                        borderRadius: BorderRadius.only(
-                            topRight: Radius.circular(10),
-                            topLeft: Radius.circular(10))),
-                  ),
-                  GestureDetector(
-                    onPanUpdate: (tapInfo) {
-                      if (!spotifyFS) {
-                        setState(() {
-                          position = Offset(position.dx + tapInfo.delta.dx,
-                              position.dy + tapInfo.delta.dy);
-                        });
-                      }
-                    },
-                    onPanStart: (details) {
-                      Provider.of<OnOff>(context, listen: false).onSpotifyPan();
-                    },
-                    onPanEnd: (details) {
-                      Provider.of<OnOff>(context, listen: false).offSpotifyPan();
-                    },
-                    onDoubleTap: () {
-                      Provider.of<OnOff>(context, listen: false).toggleSpotifyFS();
-                    },
-                    child: Container(
-                      alignment: Alignment.topRight,
-                      width: spotifyFS
-                          ? screenWidth(context, mulBy: 0.95)
-                          : screenWidth(context, mulBy: 0.7),
-                      height: spotifyFS
-                          ? screenHeight(context, mulBy: 0.059)
-                          : screenHeight(context, mulBy: 0.06),
-                      decoration: BoxDecoration(
-                          color: Colors.transparent,
-                          border: Border(
-                              bottom: BorderSide(
-                                  color: Colors.black.withOpacity(0.5),
-                                  width: 0.8))),
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.symmetric(
-                        horizontal: screenWidth(context, mulBy: 0.013),
-                        vertical: screenHeight(context, mulBy: 0.01)),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Row(
-                          children: [
-                            InkWell(
-                              child: Container(
-                                height: 11.5,
-                                width: 11.5,
-                                decoration: BoxDecoration(
-                                  color: Colors.redAccent,
-                                  shape: BoxShape.circle,
-                                  border: Border.all(
-                                    color: Colors.black.withOpacity(0.2),
-                                  ),
-                                ),
-                              ),
-                              onTap: () {
-                                Provider.of<OnOff>(context, listen: false)
-                                    .offSpotifyFS();
-                                Provider.of<Apps>(context, listen: false).closeApp("spotify");
-                                Provider.of<OnOff>(context, listen: false).toggleSpotify();
-
-
-                              },
-                            ),
-                            SizedBox(
-                              width: screenWidth(context, mulBy: 0.005),
-                            ),
-                            InkWell(
-                              onTap: (){
-                                Provider.of<OnOff>(context, listen: false).toggleSpotify();
-                                Provider.of<OnOff>(context, listen: false).offSpotifyFS();
-                              },
-                              child: Container(
-                                height: 11.5,
-                                width: 11.5,
-                                decoration: BoxDecoration(
-                                  color: Colors.amber,
-                                  shape: BoxShape.circle,
-                                  border: Border.all(
-                                    color: Colors.black.withOpacity(0.2),
-                                  ),
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              width: screenWidth(context, mulBy: 0.005),
-                            ),
-                            InkWell(
-                              child: Container(
-                                height: 11.5,
-                                width: 11.5,
-                                decoration: BoxDecoration(
-                                  color: Colors.green,
-                                  shape: BoxShape.circle,
-                                  border: Border.all(
-                                    color: Colors.black.withOpacity(0.2),
-                                  ),
-                                ),
-                              ),
-                              onTap: () {
-                                Provider.of<OnOff>(context, listen: false)
-                                    .toggleSpotifyFS();
-                              },
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+        child: Column(
+          children: [
+            SizedBox(
+              height: screenHeight(context, mulBy: 0.04),
+            ),
+            Expanded(
+              child: HtmlElementView(
+                viewType: 'spotifyIframe',
               ),
-              Expanded(
-                child: ClipRRect(
-                  borderRadius: BorderRadius.only(
-                      bottomRight: Radius.circular(10),
-                      bottomLeft: Radius.circular(10)),
-                  child: BackdropFilter(
-                    filter: ui.ImageFilter.blur(sigmaX: 30.0, sigmaY: 30.0),
-                    child: Container(
-                      height: screenHeight(context, mulBy: 0.14),
-                      width: screenWidth(
-                        context,
-                      ),
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).dividerColor.withOpacity(0.8),
-                      ),
-                      child: HtmlElementView(
-                        viewType: 'spotifyIframe',
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-
-
-    Visibility(
-      visible: topApp != "Spotify",
-      child: InkWell(
-        onTap: (){
-          Provider.of<Apps>(context, listen: false)
-              .bringToTop(ObjectKey("spotify"));
-        },
-        child: Container(
-          width: screenWidth(context,),
-          height: screenHeight(context,),
-          color: Colors.transparent,
+            ),
+            SizedBox(
+              height: screenHeight(context, mulBy: 0.05),
+            ),
+          ],
         ),
-      ),
-    ),
-        ],
       ),
     );
   }
