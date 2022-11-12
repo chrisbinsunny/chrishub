77b65f7ce421ab3954851dc9e9c0c007d9c582d8
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-11-12 23:42:44 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'dart:html';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mac_dt/system/folders.dart';
 import 'package:mac_dt/providers.dart';
 import 'package:mac_dt/sizes.dart';
+import 'firebase_options.dart';
 import 'system/openApps.dart';
 import 'theme/theme.dart';
 import 'package:mac_dt/system/componentsOnOff.dart';
@@ -12,8 +14,14 @@ import 'package:provider/provider.dart';
 import 'system/desktop.dart';
 import 'dart:html' as html;
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   window.document.onContextMenu.listen((evt) => evt.preventDefault());
+
+
+  ///Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+
   runApp(ChangeNotifierProvider<ThemeNotifier>(
       create: (_) => ThemeNotifier(ThemeNotifier.darkTheme),
       child: MyApp()));
