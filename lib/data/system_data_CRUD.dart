850f0a27e5f5556a995b9300b44c7476290661c4
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-11-14 00:49:51 +0530

    Update system_data_CRUD.dart

--- lib/data/system_data_CRUD.dart
+++ lib/data/system_data_CRUD.dart
@@ -3,9 +3,11 @@
 import 'dart:developer';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:mac_dt/components/wallpaper.dart';
 import 'package:mac_dt/data/system_data.dart';
+import 'package:mac_dt/theme/theme.dart';
 import 'package:provider/provider.dart';
 
 import '../providers.dart';
@@ -15,22 +17,9 @@ class SystemDataCRUD{
   static Box<SystemData> getSystemData() =>
       Hive.box<SystemData>('systemData');
 
-  static void startSystem(BuildContext context){
 
-    final box= getSystemData();
-    SystemData? systemData= box.get(
-        'systemData',
-        defaultValue: SystemData(
-            dark: true,
-          wallpaper: WallData(name: "Big Sur Illustration", location: "assets/wallpapers/bigsur_.jpg")
-        ));
-
-    Provider.of<DataBus>(context,
-        listen: false)
-        .setWallpaper(systemData!.wallpaper!);
-}
-
-
+///Gets data from local database. If the database is empty(i.e being run the first time),
+  ///then defaultValue is used.
   static WallData getWallpaper(){
 
     final box= getSystemData();
@@ -42,9 +31,12 @@ class SystemDataCRUD{
         )
     );
 
-    return systemData!.wallpaper!;
+    return systemData!.wallpaper;
   }
 
+  ///Sets data to local database. If the database is empty(i.e being run the first time),
+  ///then defaultValue is used. The change is being made to the value and is saved again.
+  ///WallData had to be registered as Adapter to this to work.
   static setWallpaper(WallData? wallData){
 
     final box= getSystemData();
@@ -59,4 +51,42 @@ class SystemDataCRUD{
     systemData.save();
   }
 
+
+  ///Gets data from local database. If the database is empty(i.e being run the first time),
+  ///then defaultValue is used.
+  static ThemeData getTheme(){
+
+    final box= getSystemData();
+    SystemData? systemData= box.get(
+        'systemData',
+        defaultValue: SystemData(
+            dark: true,
+            wallpaper: WallData(name: "Big Sur Illustration", location: "assets/wallpapers/bigsur_.jpg")
+        )
+    );
+
+    if(systemData!.dark)
+      return ThemeNotifier.darkTheme;
+    else
+      return ThemeNotifier.lightTheme;
+  }
+
+  ///Sets data to local database. If the database is empty(i.e being run the first time),
+  ///then defaultValue is used. The change is being made to the value and is saved again.
+  static setTheme(ThemeData themeData){
+
+    final box= getSystemData();
+    SystemData? systemData= box.get(
+        'systemData',
+        defaultValue: SystemData(
+            dark: true,
+            wallpaper: WallData(name: "Big Sur Illustration", location: "assets/wallpapers/bigsur_.jpg")
+        )
+    );
+    if(themeData==ThemeNotifier.darkTheme)
+      systemData!.dark= true;
+    else
+      systemData!.dark= false;
+    systemData.save();
+  }
 }
