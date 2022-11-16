2f34e1ace092b56d4f9e478591b39df33591cab7
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-11-16 12:47:09 +0530

    Update notification.dart

--- lib/components/notification.dart
+++ lib/components/notification.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mac_dt/system/componentsOnOff.dart';
@@ -6,6 +8,7 @@ import 'package:mac_dt/theme/theme.dart';
 import 'package:mac_dt/widgets.dart';
 import 'package:provider/provider.dart';
 import 'dart:ui' as ui;
+import 'dart:html' as html;
 
 import '../sizes.dart';
 
@@ -26,6 +29,7 @@ class _NotificationsState extends State<Notifications> {
     themeNotifier = Provider.of<ThemeNotifier>(context);
     notification = Provider.of<DataBus>(context).getNotification;
     notificationOn = Provider.of<OnOff>(context).getNotificationOn;
+    log(screenWidth(context, mulBy: 0.17).toString());
     return AnimatedPositioned(
       duration: Duration(milliseconds: 500),
       right: notificationOn?screenWidth(context, mulBy: 0.01):-screenWidth(context, mulBy: 0.201),
@@ -44,100 +48,109 @@ class _NotificationsState extends State<Notifications> {
         child: Stack(
           clipBehavior: Clip.none,
           children: [
-            AnimatedContainer(
-              duration: Duration(milliseconds: 200),
-              width: screenWidth(context, mulBy: 0.2)+0.5,
-              height: screenHeight(context, mulBy: 0.1)+0.5,
-              decoration: BoxDecoration(
-                border: Border.all(color: Theme.of(context).shadowColor, width: 1),
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.1),
-                    spreadRadius: 5,
-                    blurRadius: 10,
-                    offset: Offset(0, 3),
-                  ),
-                ],
-              ),
-              child: Container(
-                width: screenWidth(context, mulBy: 0.2),
-                height: screenHeight(context, mulBy: 0.1),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                  border: Border.all(
-                      color: Colors.grey.withOpacity(0.9),
-                      width: themeNotifier.isDark()?0.5:0
-                  ),
-
+            InkWell(
+              onTap: (){
+                html.window.open(notification["url"]!, 'new tab');
+              },
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 200),
+                width: 270.5,
+                height: 75.5,
+                constraints: BoxConstraints(
+                    minHeight: 80
                 ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: BackdropFilter(
-                    filter: ui.ImageFilter.blur(sigmaX: 30.0, sigmaY: 30.0),
-                    child: Container(
-                      height: screenHeight(context, mulBy: 0.14),
-                      width: screenWidth(
-                        context,
-                      ),
-                      padding: EdgeInsets.only(
-                          left: screenWidth(context, mulBy: 0.005),
-                          right: screenWidth(context, mulBy: 0.01),
-                          top: screenHeight(context, mulBy: 0.01),
-                          bottom: screenHeight(context, mulBy: 0.01)
-                      ),
-                      decoration: BoxDecoration(
-                          color: Theme.of(context).focusColor
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            children: [
-                              Container(
-                                height: screenHeight(context, mulBy: 0.027),
-                                child: Image.asset(
-                                  "assets/appsMac/${notification["app"]}.png",
-                                ),
-                              ),
-                              SizedBox(
-                                width: 3,
-                              ),
-                              MBPText(
-                                text: notification["app"]!.toUpperCase(),
-                                color: Theme.of(context).cardColor.withOpacity(0.5),
-                                size: 10,
-                                fontFamily: "SF",
-                              ),
-                              Spacer(),
-                              Center(
-                                child: MBPText(text: "now",
-                                  color: Theme.of(context).cardColor.withOpacity(1),
-                                  size: 10,
-                                ),
-                              )
-                            ],
-                          ),
-
-                          MBPText(
-                            text: notification["head"],
-                            color: Theme.of(context).cardColor.withOpacity(1),
-                            size: 10.5,
-                            weight: Theme.of(context).textTheme.headline3!.fontWeight,
-                            maxLines: 1,
-                          ),
-                          MBPText(
-                            text: notification["notification"],
-                            color: Theme.of(context).cardColor.withOpacity(1),
-                            size: 10.5,
-                            weight: Theme.of(context).textTheme.headline2!.fontWeight,
-                            maxLines: 1,
-                          ),
-                        ],
-                      )
+                decoration: BoxDecoration(
+                  border: Border.all(color: Theme.of(context).shadowColor, width: 1),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      spreadRadius: 5,
+                      blurRadius: 10,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Container(
+                  width: 270,
+                  height: 75,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    border: Border.all(
+                        color: Colors.grey.withOpacity(0.9),
+                        width: themeNotifier.isDark()?0.5:0
                     ),
 
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: BackdropFilter(
+                      filter: ui.ImageFilter.blur(sigmaX: 30.0, sigmaY: 30.0),
+                      child: Container(
+                        height: screenHeight(context, mulBy: 0.14),
+                        width: screenWidth(
+                          context,
+                        ),
+
+                        padding: EdgeInsets.only(
+                            left: screenWidth(context, mulBy: 0.005),
+                            right: screenWidth(context, mulBy: 0.01),
+                            top: screenHeight(context, mulBy: 0.01),
+                            bottom: screenHeight(context, mulBy: 0.01)
+                        ),
+                        decoration: BoxDecoration(
+                            color: Theme.of(context).focusColor
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Container(
+                                  height: screenHeight(context, mulBy: 0.03),
+                                  child: Image.asset(
+                                    "assets/appsMac/${notification["app"]}.png",
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 3,
+                                ),
+                                MBPText(
+                                  text: notification["app"]!.toUpperCase(),
+                                  color: Theme.of(context).cardColor.withOpacity(0.5),
+                                  size: 11,
+                                  fontFamily: "SF",
+                                ),
+                                Spacer(),
+                                Center(
+                                  child: MBPText(text: "now",
+                                    color: Theme.of(context).cardColor.withOpacity(1),
+                                    size: 11,
+                                  ),
+                                )
+                              ],
+                            ),
+
+                            MBPText(
+                              text: notification["head"],
+                              color: Theme.of(context).cardColor.withOpacity(1),
+                              size: 11.5,
+                              weight: Theme.of(context).textTheme.headline3!.fontWeight,
+                              maxLines: 1,
+                            ),
+                            MBPText(
+                              text: notification["notification"],
+                              color: Theme.of(context).cardColor.withOpacity(1),
+                              size: 11.5,
+                              weight: Theme.of(context).textTheme.headline2!.fontWeight,
+                              maxLines: 1,
+                            ),
+                          ],
+                        )
+                      ),
+
+                    ),
                   ),
                 ),
               ),
