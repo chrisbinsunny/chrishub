a7295263ba6a9503e88c973a96979e98613bf309
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-11-18 15:51:38 +0530

    Update folders.dart

--- lib/system/folders/folders.dart
+++ lib/system/folders/folders.dart
@@ -19,10 +19,21 @@ class Folders extends ChangeNotifier{
   Widget? temp;
   List<Folder> folders= FoldersDataCRUD.getFolders();
 
+  List<Widget> desktopItems=[
+    InkWell(
+      onTap: null,
+      onDoubleTap: (){},
+      child: ,
+    ),
+  ];
+
   List<Folder> get getFolders {
     return folders;
   }
 
+  List<Widget> get getDesktopItems {
+    return desktopItems;
+  }
 
   void createFolder(context, {String name="untitled folder", bool? renaming}){
     Offset initPos=  Offset(200, 150);
@@ -366,6 +377,223 @@ class _FolderState extends State<Folder> {
                             ):BoxDecoration(),
                             child: Text(widget.name??"", textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontFamily: "HN", fontWeight: FontWeight.w500, fontSize: 12,), maxLines: 1, overflow: TextOverflow.ellipsis, ),
                           ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+class DesktopItem extends StatefulWidget {
+  String? name;
+  Offset? initPos;
+  bool? renaming;
+  bool selected;
+  late VoidCallback deSelectFolder;
+  late VoidCallback renameFolder;
+  DesktopItem({Key? key, this.name, this.initPos, this.renaming= false, this.selected=false,}): super(key: key);
+  @override
+  _DesktopItemState createState() => _DesktopItemState();
+}
+
+class _DesktopItemState extends State<DesktopItem> {
+  Offset? position= Offset(200, 150);
+  TextEditingController? controller;
+  FocusNode _focusNode = FocusNode();
+  bool pan= false;
+  bool bgVisible= false;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = new TextEditingController(text: widget.name, );
+    controller!.selection=TextSelection.fromPosition(TextPosition(offset: controller!.text.length));
+    position=widget.initPos;
+    selectText();
+    widget.renameFolder=(){
+      if (!mounted) return;
+      setState(() {
+        widget.renaming=true;
+      });
+    };
+    widget.deSelectFolder= (){
+      if (!mounted) return;
+      setState(() {
+        widget.selected=false;
+        widget.renaming=false;
+        //widget.name=controller.text.toString();
+      });
+    };
+  }
+
+  void selectText(){
+    _focusNode.addListener(() {
+      if(_focusNode.hasFocus) {
+        controller!.selection = TextSelection(baseOffset: 0, extentOffset: controller!.text.length);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if(!pan)
+      position=widget.initPos;
+    return Container(
+      height: screenHeight(context),
+      width: screenWidth(context),
+      child: Stack(
+        children: [
+          Visibility(
+            visible: bgVisible,
+            child: Positioned(
+              top: widget.initPos!.dy,
+              left: widget.initPos!.dx,
+              child: Container(
+                width: screenWidth(context, mulBy: 0.08),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      height: screenHeight(context, mulBy: 0.1),
+                      padding: EdgeInsets.symmetric(horizontal: screenWidth(context,mulBy: 0.0005)),
+                      decoration: BoxDecoration(
+                          color: Colors.black.withOpacity(0.25),
+                          border: Border.all(
+                              color: Colors.grey.withOpacity(0.4),
+                              width: 2
+                          ),
+                          borderRadius: BorderRadius.circular(4)
+                      ),
+                      child: Image.asset("assets/icons/folder.png", height: screenHeight(context, mulBy: 0.085), width: screenWidth(context, mulBy: 0.045), ),
+                    ),
+                    SizedBox(height: screenHeight(context, mulBy: 0.005), ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: screenHeight(context, mulBy: 0.024),
+                          padding: EdgeInsets.symmetric(horizontal: screenWidth(context,mulBy: 0.005)),
+                          alignment: Alignment.center,
+                          decoration:BoxDecoration(
+                              color: Color(0xff0058d0),
+                              borderRadius: BorderRadius.circular(3)
+                          ),
+                          child: Text(widget.name??"", textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontFamily: "HN", fontWeight: FontWeight.w500, fontSize: 12,), maxLines: 1, overflow: TextOverflow.ellipsis, ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          AnimatedPositioned(
+            duration: Duration(milliseconds: pan?0:200),
+            top: position!.dy,
+            left: position!.dx,
+            child: GestureDetector(
+              onPanUpdate: (tapInfo){
+                if (!mounted) return;
+                setState(() {
+                  position = Offset(position!.dx + tapInfo.delta.dx,
+                      position!.dy + tapInfo.delta.dy);
+                });
+              },
+              onPanStart: (e){
+                tapFunctions(context);
+                if (!mounted) return;
+                setState(() {
+                  widget.renaming=false;
+                  widget.selected=false;
+                  widget.name=controller!.text.toString();
+                  pan=true;
+                  bgVisible=true;
+                });
+              },
+              onPanEnd: (e){
+                if (!mounted) return;
+                setState(() {
+                  pan=false;
+                  position=widget.initPos;
+                });
+                Timer(Duration(milliseconds: 200), (){
+                  if (!mounted) return;
+                  setState(() {
+                    bgVisible=false;
+                    widget.selected=true;
+                  });});
+              },
+
+              onTap: (){
+                tapFunctions(context);
+                if (!mounted) return;
+                setState(() {
+                  widget.selected=true;
+                });
+              },
+
+              onSecondaryTap: (){
+                if (!mounted) return;
+                setState(() {
+                  widget.selected=true;
+                });
+                Provider.of<OnOff>(context, listen: false).onFRCM();
+              },
+              onSecondaryTapDown: (details){
+                tapFunctions(context);
+                Provider.of<DataBus>(context, listen: false).setPos(details.globalPosition);
+              },
+              child: Container(
+                width: screenWidth(context, mulBy: 0.08),
+
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      height: screenHeight(context, mulBy: 0.1),
+                      padding: EdgeInsets.symmetric(horizontal: screenWidth(context,mulBy: 0.0005)),
+                      decoration: (widget.renaming!||widget.selected)?BoxDecoration(
+                          color: Colors.black.withOpacity(0.25),
+                          border: Border.all(
+                              color: Colors.grey.withOpacity(0.4),
+                              width: 2
+                          ),
+                          borderRadius: BorderRadius.circular(4)
+                      ):BoxDecoration(
+                        border: Border.all(
+                            color: Colors.grey.withOpacity(0.0),
+                            width: 2
+                        ),
+                      ),
+                      child: Image.asset("assets/icons/folder.png", height: screenHeight(context, mulBy: 0.085), width: screenWidth(context, mulBy: 0.045), ),
+                    ),
+                    SizedBox(height: screenHeight(context, mulBy: 0.005), ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: screenHeight(context, mulBy: 0.024),
+                          padding: EdgeInsets.symmetric(horizontal: screenWidth(context,mulBy: 0.005)),
+                          alignment: Alignment.center,
+                          decoration: widget.selected?BoxDecoration(
+                              color: Color(0xff0058d0),
+                              borderRadius: BorderRadius.circular(3)
+                          ):BoxDecoration(),
+                          child: Text(widget.name??"", textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontFamily: "HN", fontWeight: FontWeight.w500, fontSize: 12,), maxLines: 1, overflow: TextOverflow.ellipsis, ),
+                        ),
                       ],
                     )
                   ],
