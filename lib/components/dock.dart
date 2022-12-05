651cfe29c7ebbdcf577e242c4bb9c8850387286f
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-12-05 21:52:22 +0530

    Update dock.dart

--- lib/components/dock.dart
+++ lib/components/dock.dart
@@ -231,7 +231,7 @@ class _DockerState extends State<Docker> {
                         dx: _getPath(screenWidth(context, mulBy: 0.1764), _getCursor(), ),
                         onTap: () {
                           tapFunctions(context);
-                          html.window.open('https://drive.google.com/uc?export=download&id=1lPK15gLkNr2Rso3JNr0b-RdmFN245w87', '_self');
+                          html.window.open('https://drive.google.com/uc?export=download&id=1cuIQHOhjvZfM_M74HjsICNpuzvMO0uKX', '_self');
                           Provider.of<OnOff>(context, listen: false)
                               .maxAbout();
                           Provider.of<Apps>(context, listen: false).openApp(
